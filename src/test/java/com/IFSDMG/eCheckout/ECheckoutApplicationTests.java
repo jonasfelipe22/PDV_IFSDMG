8e12cb5eacda28e3f5ea19409cafb0be0f5fbbdd
Jonas Barros <83955941+jonasfelipe22@users.noreply.github.com>
2023-10-30 08:08:16 -0300

    upload Springboot create at spring.io
    Project
    Gradle - GroovyGradle 
    Language
    Java
    Spring Boot
    3.1.5
    Project Metadata
    Group
    com.IFSDMG
    Artifact
    eCheckout
    Name
    eCheckout
    Description
    Project PDV
    Package name
    com.IFSDMG.eCheckout
    Packaging
    Jar
    Java
    21

---
+++ src/test/java/com/IFSDMG/eCheckout/ECheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IFSDMG.eCheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
